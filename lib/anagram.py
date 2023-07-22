7bee49f9dd63994f9087f8d31fd498d2c2c23484
Christian Lopez <lopezchristian1610@gmail.com>
2023-07-22 01:44:41 -0400

    All anagram tests passed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, list):
+        word_letters = [letter for letter in self.word]
+        anagram_matches = []
+
+        for word in list:
+            list_letters = [letter for letter in word]
+            if sorted(word_letters) == sorted(list_letters):
+                anagram_matches.append(word)
+
+        if len(anagram_matches) > 0:
+            return anagram_matches
+        else:
+            return []
